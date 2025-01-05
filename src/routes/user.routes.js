cf78917f6a34627fb098b26b35e56d7e31c045ae
fieldmarcel <shiva9590t@gmail.com>
2025-01-05 23:43:07 +0530

    added another route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { logoutUser,loginUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js"; // Adjust the path as needed
+import { logoutUser, getCurrentUser,loginUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js"; // Adjust the path as needed
 import { upload } from "../middlewares/multer.middleware.js";
 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
